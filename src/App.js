581d8de1e0560ffe39e222711371509786553f1f
KairstenFay <kairsten.fay@gmail.com>
2019-03-14 10:50:07 -0700

    Added google analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './components/QuoteBox';
+import Logo from './components/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <header className="App-header">
-            </header>
-        <QuoteBox />
+          </header>
+          <QuoteBox />
+          <Logo />
       </div>
     );
   }
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = function() {
+
+    return (
+        <footer>
+            <form action="http://kairstenfay.github.io/">
+                <button id="logo" type="submit" value="Go to Kairsten's website">More by Kairsten</button>
+            </form>
+        </footer>
+    );
+};
+
+export default Logo;
